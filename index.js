57f19be38ba8ad3afcf657206812877e0d8db705
OthiRaja <othiraja5677@gmail.com>
2024-09-13 18:01:38 +0530

    "Uncommented code blocks in index.html and updated countdown date in index.js"

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ document.querySelector("#hide").style.display = "none";
 
 // Set the date we're counting down to
 // use new Date(new Date().getTime() + 7000).getTime() for testing (1min)
+// const countdownDate =new Date(new Date().getTime() + 7000).getTime();
 const countdownDate = new Date("Oct 20, 2024 00:00:00").getTime();
+
 // Update the countdown every 1 second
 const countdownInterval = setInterval(function() {
   // Get the current date and time
